f3142c833808658fbd771fbabae10ab0f66b3611
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-12-22 17:37:31 +0530

    Update start.py

--- SHUKLAMUSIC/utils/inline/start.py
+++ SHUKLAMUSIC/utils/inline/start.py
@@ -16,7 +16,7 @@ def start_panel(_):
     return buttons
 
 
-def private_panel(_):
+ddef private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
@@ -26,11 +26,12 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_5"], url=f"https://t.me/Itz_alpha_dude"),
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/Backtofireda"),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
